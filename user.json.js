a49f27fb1dd3946166cadc70e53d807922dec000
nicolas@malservet.eu <nicolas@malservet.eu>
2014-10-01 17:04:10 +0200

    add comment to specify the mongo version of this script

--- user.json.js
+++ user.json.js
@@ -1,7 +1,6 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * command to create the admin user for this db.
+ * only available since mongo 2.6
  */
 
 db.createUser(
